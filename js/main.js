d48529faf95c77e271b7128283ecaf90b94d8117
Medfix ads <medfixads@gmail.com>
2025-01-05 19:31:38 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+var typed = new Typed(".text",{
+	strings:['We Design You Shine.','Life to Your Brand','Your Vision, Our Priority','Elevate Your Brand', 'Elevate Your Business'],
+	typeSpeed: 100,
+	backSpeed: 40,
+	backDelay: 100,
+	loop: true,
+	showCursor: false,
+  
+	});
+	
